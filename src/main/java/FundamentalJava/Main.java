068fa75c41070370d5e305a14a9914fa241e015f
Annamalai1995 <s.annamalai1995@gmail.com>
2023-07-18 14:03:39 +0530

    Array

--- src/main/java/FundamentalJava/Main.java
+++ src/main/java/FundamentalJava/Main.java
@@ -1,7 +1,21 @@
 package FundamentalJava;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hello world!");
+        System.out.println("Bhuvaneshwari");
+
+
+        Scanner ss = new Scanner(System.in);
+        System.out.println("enter first value");
+        int a = ss.nextInt();
+        System.out.println("enter second value");
+        int b=ss.nextInt();
+        int c=a+b;
+
+        System.out.println("The result:"+c);
     }
 }
