36fed7d06d755991aa4e0d7a411dd8129edbb483
Annamalai1995 <s.annamalai1995@gmail.com>
2023-07-25 15:27:46 +0530

    Array

---
+++ src/main/java/FundamentalJava/Array/InsertArray.java
@@ -0,0 +1,28 @@
+package FundamentalJava.Array;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class InsertArray {
+    public static void main(String[] args) {
+        int[] arr=new int[10];
+
+//		arr[5]=8987;
+//		arr[9]=87654;
+//		System.out.println(Arrays.toString(arr));
+
+        Scanner scan=new Scanner(System.in);
+
+        for(int i=0;i<arr.length;i+=1)
+        {
+            System.out.println("Enter your one digit number");
+            arr[i]=scan.nextInt();
+        }
+
+        for (int gaja:arr)
+        {
+            System.out.println(gaja);
+        }
+
+    }
+}
---
+++ src/main/java/FundamentalJava/Array/PassingArray.java
@@ -0,0 +1,25 @@
+package FundamentalJava.Array;
+
+import java.util.Arrays;
+
+public class PassingArray {
+    public void PassingArray(double[] salary1)
+    {
+        salary1[3]=12222.7;
+        salary1[0]=999999.9;
+        System.out.println(Arrays.toString(salary1));
+    }
+    public void PassingArray2(String name1)
+    {
+        System.out.println("Welcome");
+    }
+    public static void main(String[] args) {
+        int a=100;
+        String name="Gaja";
+        double[] salary= {12.3,5.6,4.3,3.2,2.4};
+        PassingArray pass=new PassingArray();
+        pass.PassingArray(salary);
+        pass.PassingArray2("Gajaraj");
+
+    }
+}
