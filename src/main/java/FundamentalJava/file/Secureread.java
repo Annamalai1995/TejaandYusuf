72ecbecf0ca27c8fdb38c7357ee77769abc2353f
Annamalai1995 <s.annamalai1995@gmail.com>
2023-08-01 15:18:54 +0530

    Collectionandfiles completed

---
+++ src/main/java/FundamentalJava/file/Secureread.java
@@ -0,0 +1,22 @@
+package FundamentalJava.file;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.zip.InflaterInputStream;
+
+public class Secureread {
+    public static void main(String[] args) throws IOException {
+        File file= new File("F:\\Data.txt");
+        FileInputStream fis= new FileInputStream(file);
+        InflaterInputStream ifs= new InflaterInputStream(fis);
+        int size1=fis.available();
+        byte ss[]= new byte[size1];
+        ifs.read(ss);
+        System.out.println(new String(ss));
+        ifs.close();
+        fis.close();
+
+    }
+}
---
+++ src/main/java/FundamentalJava/file/writefile.java
@@ -0,0 +1,23 @@
+package FundamentalJava.file;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class writefile {
+    public static void main(String[] args) throws IOException {
+        File f = new File("F:\\Hello.txt");
+        //f.createNewFile();
+        //System.out.println(f.getName()+"successfully ");
+
+
+        FileOutputStream fos= new FileOutputStream(f);
+        Scanner ss =new Scanner(System.in);
+        System.out.println("Which you want to write the file");
+        String data=ss.nextLine();
+        fos.write(data.getBytes());
+        System.out.println(f.getName()+"has be write successfully");
+
+    }
+}
